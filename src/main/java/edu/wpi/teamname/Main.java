6b99c834776e6ef08229a8f724c8d5ed192ab0d8
Tdonnelly20 <tate.donne@gmail.com>
2022-03-20 20:41:33 -0400

    Update Main.java

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Tate Donnelly!");
-    System.out.println("Hello World!");
-    System.out.println("Trevor Ng");
     App.launch(App.class, args);
   }
 }
