de68ad903dd798cb03679b09bf24c1933e688239
jdrockmael <jdrockmael@wpi.edu>
2022-03-21 19:51:57 -0400

    Jason Rockmael

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Tate Donnelly!");
+    System.out.println("Jason Rockmael");
     App.launch(App.class, args);
   }
 }
