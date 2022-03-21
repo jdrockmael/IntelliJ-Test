e32fce9a395bc7027c8bab654cf39549b6081b4b
Tdonnelly20 <tate.donne@gmail.com>
2022-03-20 20:28:17 -0400

    Tate Donnelly

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Tate Donnelly!");
     App.launch(App.class, args);
   }
 }
